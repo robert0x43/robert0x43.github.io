622a718ffd2e462d3b658f6f70521fbd31c66966
Robert William Cerda <rwcerda@berkeley.edu>
2024-10-13 19:36:12 -0500

    reorg

---
+++ js/scripts.js
@@ -0,0 +1,25 @@
+function toggleMode() {
+    const body = document.body;
+    body.classList.toggle("dark-mode");
+  
+    if (body.classList.contains("dark-mode")) {
+      body.classList.remove("light-mode");
+    } else {
+      body.classList.add("light-mode");
+    }
+  }
+  
+  function searchGoogle() {
+    const query = document.querySelector('.search-bar').value;
+    if (query) {
+      window.location.href = `https://www.google.com/search?q=${encodeURIComponent(query + ' reddit')}`;
+    }
+  }
+  
+  // Trigger search when Enter key is pressed
+  document.querySelector('.search-bar').addEventListener('keypress', function (e) {
+    if (e.key === 'Enter') {
+      searchGoogle();
+    }
+  });
+  
