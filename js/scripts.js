4dfc1c6872804c0ed8581cfc036350c3b1db9750
Robert William Cerda <rwcerda@berkeley.edu>
2024-11-03 17:23:00 -0600

    saving theme preference

--- js/scripts.js
+++ js/scripts.js
@@ -4,10 +4,21 @@ function toggleMode() {
   
     if (body.classList.contains("dark-mode")) {
       body.classList.remove("light-mode");
+      localStorage.setItem("theme", "dark"); // Save "dark" theme to localStorage
     } else {
       body.classList.add("light-mode");
+      localStorage.setItem("theme", "light"); // Save "light" theme to localStorage
     }
   }
+  
+  // Function to apply the saved theme on page load
+  window.onload = function() {
+    const savedTheme = localStorage.getItem("theme");
+    if (savedTheme) {
+      document.body.classList.remove("light-mode", "dark-mode");
+      document.body.classList.add(savedTheme + "-mode");
+    }
+  };
   
   function searchGoogle() {
     const query = document.querySelector('.search-bar').value;
